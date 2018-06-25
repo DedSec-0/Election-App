c318c54ac5bfaff5a04206aca3c5e0b61b4957ff
Hamza Awan <hamza_awan@hotmail.com>
2018-06-25 23:21:36 +0500

    Election App
    It is a simple Election app shows the detail of all the polling stations and their location on google map. It is non-profit app made for my MAD project.

--- Election App/ViewController.swift
+++ Election App/ViewController.swift
@@ -12,7 +12,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     
+        let fileName = "myFile.txt"
+        if let dirPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+        {
+            let fileUrl = dirPath.appendingPathComponent(fileName)
+            //            do
+            //            {
+            //                try fileData.write(to: fileUrl, atomically: true, encoding: String.Encoding.utf8)
+            //            }
+            //            catch{
+            //
+            //            }
+            do
+            {
+                let data = try String.init(contentsOf: fileUrl, encoding: .utf8)
+                print("the data is: ", data)
+                
+                
+            }
+            catch{
+                
+            }
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
